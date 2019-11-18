e955c6917580d8f8458e7291103f0f16ba796c40
ladvien <honeysucklehardware@gmail.com>
2019-11-18 05:42:27 -0600

    Testing

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ladvien_ml',
+      version='0.0.1',
+      description='Ladvien\'s personal collection of machine-learning tools.',
+      url='https://github.com/Ladvien/ladvien_ml.git',
+      author='C. Thomas Brittain',
+      author_email='cthomasbrittain@hotmail.com',
+      license='MIT',
+      packages=['ladvien_ml'],
+      zip_safe=False
+)
