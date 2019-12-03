ea30b58786dece70b367424b28e365b64878b4ce
ladvien <honeysucklehardware@gmail.com>
2019-12-03 07:01:53 -0600

    Working on documentation

--- commit.py
+++ commit.py
@@ -2,6 +2,7 @@ import sys, os
 
 commit_message = input('Git message: ')
 os.system('git add .')
-os.system(f'git commit -m {commit_message}')
+os.system(f'git commit -m "{commit_message}"')
 os.system('git push')
 os.system('pip uninstall ladvien_ml -y')
+os.system('pip install git+https://github.com/Ladvien/ladvien_ml.git')
