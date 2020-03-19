c7a15518a9f32011f898d545c04adcd664c49d50
ladvien <honeysucklehardware@gmail.com>
2020-03-19 07:22:31 -0500

    Updated make_ohe_feature_readable to handle dummy trap avoidance.

--- ladvien_ml/feature_prep.py
+++ ladvien_ml/feature_prep.py
@@ -143,35 +143,45 @@ class FeaturePrep:
 
         return df
 
-    def make_ohe_feature_readable(self, feature_name, print_output = False, separate_value = False):
+    def make_ohe_feature_readable(self, feature_name, seperator = '.', print_output = False, separate_value = False):
         """
 
-            feature_name: str, name of the feature.
-            print_output: Boolean, print the processed feature name.
-            separate_value: Boolean, if true, returns a tuple containing the feature name and the value.
+                feature_name: str, name of the feature.
+                print_output: Boolean, print the processed feature name.
+                separate_value: Boolean, if true, returns a tuple containing the feature name and the value.
 
-            When one-hot encoding (OHE) values the column names often get garbled.  This method will take a
-            OHE string and return a human readable name.  The string should be of the type
+                When one-hot encoding (OHE) values the column names often get garbled.  This method will take a
+                OHE string and return a human readable name.  The string should be of the type
 
                 name_of_feature_value, e.g., 'weight_range_150'
 
-            >>> print(make_ohe_feature_readable(weight_range_150))
-            "Weight Range = 150"
+                >>> print(make_ohe_feature_readable(weight_range_150))
+                "Weight Range = 150"
         """
-        
-        feature_name = feature_name.split('_')
+
+        # When avoiding the the dummy variable trap two category variables
+        # will not have a seperator or value in the feature name.
+        if seperator not in feature_name:
+                feature_name += f'{seperator}1'
+
+        # If underscores are used instead of spaces, replace them.
+        feature_name = feature_name.replace('_', ' ')
+
+        feature_name = feature_name.split(seperator)
         value = feature_name[-1]
-        
+
+        # Use SQL like nulls.
         if value == 'nan':
-            value = 'NULL'
-            
-        if '.0' in value:
-            value = value.replace('.0', '')
+                value = 'NULL'
         
+        # If the dummy value was encoded as a float, clean it up.
+        if '.0' in value:
+                value = value.replace('.0', '')
+
+        # Put the human readable name back together.
         feature_name = feature_name[0:-1]
         feature_name = ' '.join(feature_name).title()
-    
-        full_output = f'"{feature_name}" = {value}'
+        full_output = f'{feature_name} = {value}'
 
         if print_output:
             print(full_output)
@@ -180,4 +190,5 @@ class FeaturePrep:
             return (feature_name, value)
         else:
             return full_output
+
 
