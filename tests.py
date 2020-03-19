afdf8100effdeb3f056e6398003dc8fc03324887
ladvien <honeysucklehardware@gmail.com>
2020-03-19 07:32:32 -0500

    Updated make_ohe_feautre_readable to use seperator argument.

--- tests.py
+++ tests.py
@@ -12,15 +12,16 @@ def make_ohe_feature_readable(feature_name, seperator = '.', print_output = Fals
         """
 
                 feature_name: str, name of the feature.
+                seperator: str, the character or string representing label from value.
                 print_output: Boolean, print the processed feature name.
                 separate_value: Boolean, if true, returns a tuple containing the feature name and the value.
 
                 When one-hot encoding (OHE) values the column names often get garbled.  This method will take a
                 OHE string and return a human readable name.  The string should be of the type
 
-                name_of_feature_value, e.g., 'weight_range_150'
+                name_of_feature_value, e.g., 'weight_range.150'
 
-                >>> print(make_ohe_feature_readable(weight_range_150))
+                >>> print(make_ohe_feature_readable(weight_range.150))
                 "Weight Range = 150"
         """
 
@@ -32,20 +33,17 @@ def make_ohe_feature_readable(feature_name, seperator = '.', print_output = Fals
         # If underscores are used instead of spaces, replace them.
         feature_name = feature_name.replace('_', ' ')
 
-        feature_name = feature_name.split(seperator)
-        value = feature_name[-1]
+        split_feature_name = feature_name.split(seperator)
+        print(split_feature_name)
+        value = split_feature_name[1]
 
         # Use SQL like nulls.
         if value == 'nan':
                 value = 'NULL'
         
-        # If the dummy value was encoded as a float, clean it up.
-        if '.0' in value:
-                value = value.replace('.0', '')
-
-        # Put the human readable name back together.
-        feature_name = feature_name[0:-1]
-        feature_name = ' '.join(feature_name).title()
+        # If there's a third split, we don't want it. E.g., 'neat_feature.0.0'.
+        split_feature_name = split_feature_name[0:1]  
+        feature_name = ' '.join(split_feature_name).title()
         full_output = f'{feature_name} = {value}'
 
         if print_output:
