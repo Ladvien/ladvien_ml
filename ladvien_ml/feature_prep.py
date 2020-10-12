6f9ccd06795b71f0ebd4d2d3194c506063598227
ladvien <honeysucklehardware@gmail.com>
2020-10-11 19:29:38 -0500

    Working

--- ladvien_ml/feature_prep.py
+++ ladvien_ml/feature_prep.py
@@ -8,6 +8,7 @@ Created on Thu Nov  7 07:05:13 2019
 
 import pandas as pd
 from datetime import datetime
+import numpy as np
 
 
 class FeaturePrep:
@@ -18,15 +19,12 @@ class FeaturePrep:
     def __init__(self):
         pass
         
-    def remove_unwanted_columns(self, df):
+    def remove_unwanted_columns(self, df, cols_to_remove=['Unnamed: 0', 'Unnamed: 0.1', 'looker_error']):
         """
             Drops the following columns: 'Unnamed: 0', 'Unnamed: 0.1', 'looker_error'
         """
-        cols_to_remove = ['Unnamed: 0', 'Unnamed: 0.1', 'looker_error']
-        for col in cols_to_remove:
-            if col in df:
-                df.drop(col, axis = 1, inplace = True)
-        
+        cols_to_remove = [col for col in cols_to_remove if col in df.columns.tolist()]
+        df = df.drop(cols_to_remove, axis = 1)
         return df
     
     def clean_date_feature(self, df, feature_name, fill_date = '1800-01-01', look_back = '1800-01-01', mark_weird = False):
@@ -188,4 +186,76 @@ class FeaturePrep:
         else:
             return full_output
 
+   # This code was borrowed from ArjanGroen
+    # https://www.kaggle.com/arjanso/reducing-dataframe-memory-size-by-65
+    
+    def reduce_mem_usage(self, df):
+        start_mem_usg = df.memory_usage().sum() / 1024**2 
+        print("Memory usage of properties dataframe is :",start_mem_usg," MB")
+        NAlist = [] # Keeps track of columns that have missing values filled in. 
+
+        date_cols = df.select_dtypes(include=['datetime64']).columns.tolist()
+        cols = [col for col in df.columns.tolist() if col not in date_cols]
+
+        for col in cols:
+            if df[col].dtype != object:  # Exclude strings
+                
+                # Print current column type
+                print("******************************")
+                print("Column: ",col)
+                print("dtype before: ",df[col].dtype)
+                
+                # make variables for Int, max and min
+                IsInt = False
+                mx = df[col].max()
+                mn = df[col].min()
+                
+                # Integer does not support NA, therefore, NA needs to be filled
+                if not np.isfinite(df[col]).all(): 
+                    NAlist.append(col)
+                    df[col].fillna(mn-1,inplace=True)  
+                       
+                # test if column can be converted to an integer
+                asint = df[col].fillna(0).astype(np.int64)
+                result = (df[col] - asint)
+                result = result.sum()
+                if result > -0.01 and result < 0.01:
+                    IsInt = True
+    
+                
+                # Make Integer/unsigned Integer datatypes
+                if IsInt:
+                    if mn >= 0:
+                        if mx < 255:
+                            df[col] = df[col].astype(np.uint8)
+                        elif mx < 65535:
+                            df[col] = df[col].astype(np.uint16)
+                        elif mx < 4294967295:
+                            df[col] = df[col].astype(np.uint32)
+                        else:
+                            df[col] = df[col].astype(np.uint64)
+                    else:
+                        if mn > np.iinfo(np.int8).min and mx < np.iinfo(np.int8).max:
+                            df[col] = df[col].astype(np.int8)
+                        elif mn > np.iinfo(np.int16).min and mx < np.iinfo(np.int16).max:
+                            df[col] = df[col].astype(np.int16)
+                        elif mn > np.iinfo(np.int32).min and mx < np.iinfo(np.int32).max:
+                            df[col] = df[col].astype(np.int32)
+                        elif mn > np.iinfo(np.int64).min and mx < np.iinfo(np.int64).max:
+                            df[col] = df[col].astype(np.int64)    
+                
+                # Make float datatypes 32 bit
+                else:
+                    df[col] = df[col].astype(np.float32)
+                
+                # Print new column type
+                print("dtype after: ",df[col].dtype)
+                print("******************************")
+        
+        # Print final result
+        print("___MEMORY USAGE AFTER COMPLETION:___")
+        mem_usg = df.memory_usage().sum() / 1024**2 
+        print("Memory usage is: ",mem_usg," MB")
+        print("This is ",100*mem_usg/start_mem_usg,"% of the initial size")
+        return df, NAlist
 
